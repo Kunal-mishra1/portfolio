937ef679125953fecc7db1e4941e4caa32390f9f
kunal mishra <kunalmishra@kunals-iMac.local>
2025-05-15 14:30:00 -0700

    Initial portfolio prototype

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
